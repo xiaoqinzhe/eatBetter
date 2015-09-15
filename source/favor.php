2683610fa2b23120c71b092e58b141e73bdf6e64
xiaoqinzhe <761534426@qq.com>
2015-09-15 22:01:50 +0800

    t

--- source/favor.php
+++ source/favor.php
@@ -4,23 +4,39 @@ require_once('common/common_func.php');
 require_once('../lib/Db.class.php');
 require_once('../lib/Log.class.php');
 
-/* $_POST['comment_id']=1;
+$_POST['comment_id']=1;
 $_POST['user_id']=1;
-$_POST['food_id']=4; */
+$_POST['recommend_id']=1;
+$_POST['favor']='true';
+
 
 if(isset($_GET['which'])){
 	if($_GET['which']=='comment'){
-		if(isset($_POST['comment_id'])){
-			$sql="update food_comments set favor=favor+1 
-			where comment_id={$_POST['comment_id']};";	
+		if(isset($_POST['comment_id'])&&isset($_POST['user_id'])&&isset($_POST['favor'])){
+			if($_POST['favor']=='true'){
+				$sql="update food_comments set favor=favor+1 
+					where comment_id={$_POST['comment_id']};";
+				$sql2="insert into food_comments_favor values({$_POST['comment_id']},{$_POST['user_id']});";
+			}else {
+				$sql="update food_comments set favor=favor-1
+				where comment_id={$_POST['comment_id']};";
+				$sql2="delete from food_comments_favor where comment_id={$_POST['comment_id']} and user_id={$_POST['user_id']};";
+			}
 		}else{
 			echo getJsonResponse(2,"post参数错误",null);
 			exit();
 		}
 	}elseif ($_GET['which']=='recommendedfood'){
-		if(isset($_POST['user_id'])&&isset($_POST['food_id'])){
-			$sql="update recommendedfood set favor=favor+1 
-			where user_id={$_POST['user_id']} and food_id={$_POST['food_id']};";
+		if(isset($_POST['user_id'])&&isset($_POST['recommend_id'])&&isset($_POST['favor'])){
+			if($_POST['favor']=='true'){
+				$sql="update recommendedfood set favor=favor+1
+				where recommend_id={$_POST['recommend_id']};";
+				$sql2="insert into recommendedfood_favor values({$_POST['recommend_id']},{$_POST['user_id']});";
+			}else{
+				$sql="update recommendedfood set favor=favor-1
+				where recommend_id={$_POST['recommend_id']};";
+				$sql2="delete from recommendedfood_favor where recommend_id={$_POST['recommend_id']} and user_id={$_POST['user_id']};";
+			}
 		}else{
 			echo getJsonResponse(2,"post参数错误",null);
 			exit();
@@ -40,12 +56,21 @@ try {
 	if($db->execute($sql)!==false){
 		if($db->numRows==0)
 			echo getJsonResponse(2,"post id错误",null);
-		else 
+	}else{
+		echo getJsonResponse(1,$db->error,null);
+		Log::error_log('database error：'.$db->error.' in '.basename(__FILE__));     //错误日志
+		exit();
+	}
+	if($db->execute($sq2)!==false){
+		if($db->numRows==0)
+			echo getJsonResponse(2,"post id错误",null);
+		else
 			echo getJsonResponse(0,"success",null);
 	}else{
 		echo getJsonResponse(1,$db->error,null);
 		Log::error_log('database error：'.$db->error.' in '.basename(__FILE__));     //错误日志
 	}
+	$db->close();
 } catch (Exception $e) {
 	echo getJsonResponse(1,"数据库连接错误",null);
 	Log::error_log("数据库连接错误");
